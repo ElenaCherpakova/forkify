af49101f206691337bc538654d85916cfafcf64f
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-12-15 19:29:27 -0500

    create controller, view and model files for splitting the logic, refactor for MVC

---
+++ src/js/model.js
@@ -0,0 +1,34 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async id => {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    const { recipe } = data.data;
+    // console.log(recipe)
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err} 💥💥💥💥💥💥💥`);
+    throw err;
+  }
+};

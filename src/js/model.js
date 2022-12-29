2fd3fe3329d42952e51a9f7b0782791eaec8af12
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-12-29 00:28:15 -0500

    fix in map function

--- src/js/model.js
+++ src/js/model.js
@@ -120,8 +120,7 @@ export const uploadRecipe = async function (newRecipe) {
     const ingredients = Object.entries(newRecipe)
       .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
       .map(ing => {
-        const ingArr = entry[1].split(',').map(el => el.trim())
-        // const ingArr = ing[1].replaceAll(' ', '').split(',');
+        const ingArr = ing[1].split(',').map(el => el.trim())
         if (ingArr.length !== 3)
           throw new Error(
             'Wrong ingredient format! Please use the correct format'
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,15 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} } data The data to be rendered
+   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render = false
+   * @this {Object} View instance
+   * @author Elena Cherpakova
+   */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
@@ -11,7 +20,7 @@ export default class View {
     const markup = this._generateMarkup();
 
     if (!render) return markup;
-    
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
